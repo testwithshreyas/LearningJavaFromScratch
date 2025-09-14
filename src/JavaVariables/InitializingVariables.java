4b7f330ea418935e9b5a8bb978d06e2f9bde0aee
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-14 22:17:54 +0530

    Added rules to declare variables

--- src/JavaVariables/InitializingVariables.java
+++ src/JavaVariables/InitializingVariables.java
@@ -1,6 +1,13 @@
 package JavaVariables;
 
-// Variables are containers for storing data values.
+/* Definition: Variables are containers for storing data values.
+The general rules for naming variables are:
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names should start with a lowercase letter, and cannot contain whitespace.
+Names can also begin with $ and _
+Names are case-sensitive ("myVar" and "myvar" are different variables).
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names. */
 
 public class InitializingVariables {
     public static void main(String[] args) {
