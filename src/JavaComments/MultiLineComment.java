bc576fec05b6dd752663ea06fbc99ca5aee0750f
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-14 21:52:40 +0530

    Multiline Comment

---
+++ src/JavaComments/MultiLineComment.java
@@ -0,0 +1,13 @@
+package JavaComments;
+
+// Multi-line comments start with /* and ends with */.
+// Any text between /* and */ will be ignored by Java.
+
+public class MultiLineComment {
+    public static void main(String[] args) {
+        /*
+        This is MultiLine Comment
+         */
+        System.out.println("Hello World!");
+    }
+}
