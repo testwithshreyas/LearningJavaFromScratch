7e98b86e574309787ee7c54c24045b447f947e24
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-14 22:07:42 +0530

    Initializing Variables

---
+++ src/JavaVariables/InitializingVariables.java
@@ -0,0 +1,14 @@
+package JavaVariables;
+
+// Variables are containers for storing data values.
+
+public class InitializingVariables {
+    public static void main(String[] args) {
+
+        // Syntax: type variableName = value;
+        String name = "Tony Stark";
+        int age = 60;
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+}
