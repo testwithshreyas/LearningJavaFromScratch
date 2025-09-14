e6af8211e9793dc5cf4082ef282c9bf9dd232587
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-14 21:36:59 +0530

    Initial Commit

---
+++ src/JavaComments/SingleLineComment.java
@@ -0,0 +1,12 @@
+package JavaComments;
+
+// Single-line comments start with two forward slashes (//).
+// Any text between // and the end of the line is ignored by Java (will not be executed).
+
+public class SingleLineComment {
+    public static void main(String[] args) {
+
+        // This is Hello World Program
+        System.out.println("Hello World!");
+    }
+}
