ac1f57c939f17c658895fe5ef14ae784c73eff8b
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-15 13:26:21 +0530

    Non-primitive data type: String

---
+++ src/JavaDataTypes/Strings.java
@@ -0,0 +1,14 @@
+package JavaDataTypes;
+/*
+Non-primitive data types are called reference types because they refer to objects.
+Non-primitive types typically starts with an uppercase letter (like String).
+Non-primitive types can be null.
+Examples of non-primitive types are Strings, Arrays, Classes etc.
+ */
+public class Strings {
+    public static void main(String[] args) {
+
+        String message = "Welcome to Java programming.";
+        System.out.println("You have one message: " + message);
+    }
+}
