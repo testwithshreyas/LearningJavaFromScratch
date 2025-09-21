07bfd764cde7208f365a4f6d09bb317f2da6fbdf
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-21 11:16:02 +0530

    Added Widening Casting Program

---
+++ src/JavaTypeCasting/WideningCasting.java
@@ -0,0 +1,12 @@
+package JavaTypeCasting;
+
+public class WideningCasting {
+    public static void main(String[] args) {
+
+        int myInt = 100;
+        double myDouble = myInt;    // Automatic casting: int to double
+
+        System.out.println("Integer Value: "+myInt);
+        System.out.println("Double Value: "+myDouble);
+    }
+}
