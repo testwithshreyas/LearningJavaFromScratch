382aa2cb4ef7a7a72e4e1a255a6b1dc787250c79
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-27 14:22:08 +0530

    Added String Trim Method Program

---
+++ src/JavaStrings/StringTrimMethod.java
@@ -0,0 +1,11 @@
+// The trim() method removes whitespace from the beginning and the end of a string.
+
+package JavaStrings;
+
+public class StringTrimMethod {
+    public static void main(String[] args) {
+
+        String country = "   India    ";
+        System.out.println("After applying trim method: "+country.trim());
+    }
+}
