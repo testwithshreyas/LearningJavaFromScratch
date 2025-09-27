ede548b6878e434bd8b79b9a37069696ad60f45d
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-27 14:11:31 +0530

    Added String Char At Method Program

---
+++ src/JavaStrings/StringCharAtMethod.java
@@ -0,0 +1,12 @@
+// You can use the charAt() method to access a character at a specific position in a string.
+
+package JavaStrings;
+
+public class StringCharAtMethod {
+    public static void main(String[] args) {
+
+        String name = "Tony Stark";
+        System.out.println("Character at 0th index position: "+name.charAt(0));
+        System.out.println("Character at 5th index position: "+name.charAt(5));
+    }
+}
