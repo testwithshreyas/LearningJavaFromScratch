1cf4671edb630edf1fad25ad4c229b3b92402b47
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-27 13:40:07 +0530

    Added String Length Method Program

---
+++ src/JavaStrings/StringLengthMethod.java
@@ -0,0 +1,11 @@
+// You can find the length of a string with the length() method.
+
+package JavaStrings;
+
+public class StringLengthMethod {
+    public static void main(String[] args) {
+
+        String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("Alphabets string lenght: "+alphabets.length());
+    }
+}
