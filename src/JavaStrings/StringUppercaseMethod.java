cc24339b5d3aa3fa31b397a2590471d9eb39500d
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-27 13:56:27 +0530

    Added String Uppercase Method Program

---
+++ src/JavaStrings/StringUppercaseMethod.java
@@ -0,0 +1,11 @@
+// The toUpperCase() method converts a string to upper case letters.
+
+package JavaStrings;
+
+public class StringUppercaseMethod {
+    public static void main(String[] args) {
+
+        String sentence = "i love coding in java language";
+        System.out.println("Uppercase conversion: "+sentence.toUpperCase());
+    }
+}
