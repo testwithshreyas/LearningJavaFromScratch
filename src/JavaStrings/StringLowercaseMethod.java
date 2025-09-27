c2718d7220ba6c7b735f7fbc5176d3248574e08f
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-27 13:59:42 +0530

    Added String Lower Method Program

---
+++ src/JavaStrings/StringLowercaseMethod.java
@@ -0,0 +1,11 @@
+// The toLowerCase() method converts a string to lower case letters.
+
+package JavaStrings;
+
+public class StringLowercaseMethod {
+    public static void main(String[] args) {
+
+        String sentence = "I LOVE EATING HEALTHY FOOD";
+        System.out.println("Lowercase conversion: "+sentence.toLowerCase());
+    }
+}
