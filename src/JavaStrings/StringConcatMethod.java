cb5e56412c4f2b64022dfa6e0b4ba0d85c11b1ce
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-09-28 11:22:18 +0530

    Added String Concat Method Program

---
+++ src/JavaStrings/StringConcatMethod.java
@@ -0,0 +1,14 @@
+// You can also use the concat() method to concatenate strings.
+
+package JavaStrings;
+
+public class StringConcatMethod {
+    public static void main(String[] args) {
+
+        String first_name = "Steve";
+        String last_name = "Rogers";
+        String full_name = first_name.concat(last_name);
+
+        System.out.println("Full name: "+full_name);
+    }
+}
