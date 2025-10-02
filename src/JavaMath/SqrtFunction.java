fb585f7dc047e29ac150411c152f26ef19ac8208
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-10-02 15:34:20 +0530

    Added sqrt Math function program

---
+++ src/JavaMath/SqrtFunction.java
@@ -0,0 +1,13 @@
+// The Math.sqrt(x) method returns the square root of x.
+
+package JavaMath;
+
+public class SqrtFunction {
+    public static void main(String[] args) {
+
+        int x = 5;
+        double square_root = Math.sqrt(25);
+
+        System.out.println("Square root of " + x + " : " + square_root);
+    }
+}
