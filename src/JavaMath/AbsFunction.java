5162befdea020f6bfa2387a2f5781307071851b2
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-10-02 15:39:29 +0530

    Added abs Math function program

---
+++ src/JavaMath/AbsFunction.java
@@ -0,0 +1,13 @@
+// The Math.abs(x) method returns the absolute (positive) value of x.
+
+package JavaMath;
+
+public class AbsFunction {
+    public static void main(String[] args) {
+
+        double x = -100.50;
+        double positive_number = Math.abs(x);
+
+        System.out.println("Absolute positive value for " + x + " : " + positive_number);
+    }
+}
