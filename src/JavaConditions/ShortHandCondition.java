dcfacb86964a82141b462c7b3b35b6edfb2ab8a1
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-10-25 14:10:16 +0530

    Added program for ShortHand Operator

---
+++ src/JavaConditions/ShortHandCondition.java
@@ -0,0 +1,11 @@
+package JavaConditions;
+
+public class ShortHandCondition {
+    public static void main(String[] args) {
+
+        int time = 20;
+        String result = (time < 18) ? "Good Day." : "Good Evening.";
+
+        System.out.println(result);
+    }
+}
