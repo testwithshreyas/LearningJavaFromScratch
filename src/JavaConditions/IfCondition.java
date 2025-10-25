ef840770b0ec61670fe3cb3f68936915f125f9f2
testwithshreyas <shreyasddeshmukh@gmail.com>
2025-10-25 13:11:11 +0530

    Added Program for If Condition

---
+++ src/JavaConditions/IfCondition.java
@@ -0,0 +1,14 @@
+//If it rains, take an umbrella. Otherwise, do nothing.
+
+package JavaConditions;
+
+public class IfCondition {
+    public static void main(String[] args) {
+
+        var IsRaining = true;
+
+        if(IsRaining){
+            System.out.println("Take an Umbrella!");
+        }
+    }
+}
